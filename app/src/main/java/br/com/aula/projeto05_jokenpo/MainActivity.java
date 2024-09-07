9b9a6e3dc94282b9fe8c8a3139a9e7dc0cbfd6a4
Lucas Martins <lucasmartins1096@hotmail.com>
2024-09-07 10:37:40 -0300

    modificaçãoes

--- app/src/main/java/br/com/aula/projeto05_jokenpo/MainActivity.java
+++ app/src/main/java/br/com/aula/projeto05_jokenpo/MainActivity.java
@@ -1,12 +1,18 @@
 package br.com.aula.projeto05_jokenpo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,57 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void selectPedra(View view){
+      this.opcaoSelecionada("pedra");
+
+    }
+    public void selectPapel(View view){
+        this.opcaoSelecionada("papel");
+    }
+    public void selectTesoura(View view){
+        this.opcaoSelecionada("tesoura");
+
+    }
+    public void opcaoSelecionada(String opcaoSelicionada){
+        ImageView imagemResultado = findViewById(R.id.imagePadrao);
+        TextView textoResultado = findViewById(R.id.textResultado);
+
+        // Lógica de Escolha da maquina (app):
+        int numero = new Random().nextInt(3);
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+        String opcaoApp = opcoes[numero];
+
+        // Mudando a Imagem da escolha do app (conforme lófica do jogo):
+        switch (opcaoApp){
+            case "pedra":
+                imagemResultado.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imagemResultado.setImageResource(R.drawable.papel);
+                break;
+            case"tesoura":
+                imagemResultado.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+        // Aplicar Lógica do jogo (Jo-ken-po):
+        if(// App ganhando
+                (opcaoApp == "pedra" && opcaoSelicionada == "tesoura") ||
+                        (opcaoApp == "papel" && opcaoSelicionada == "pedra") ||
+                        (opcaoApp == "tesoura" && opcaoSelicionada == "papel")
+        ) {
+            textoResultado.setText(R.string.appJogoGameOver);
+        }else if (// jogador ganhando
+                (opcaoSelicionada == "pedra" && opcaoApp == "tesoura") ||
+                        (opcaoSelicionada == "papel" && opcaoApp == "pedra") ||
+                        (opcaoSelicionada == "tesoura" && opcaoApp == "papel")
+        ) {
+            textoResultado.setText(R.string.appJogoWin);
+        }else{// Empate
+            textoResultado.setText(R.string.appJogoEmpate);
+
+        }
     }
 }
